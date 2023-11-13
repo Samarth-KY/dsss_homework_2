1955af8952e55aaa2e4df369398dbf2d13f825ea
Samarth-KY <141041904+Samarth-KY@users.noreply.github.com>
2023-11-13 21:19:36 +0100

    Added setup.py, requirements.txt and __init__.py files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup 
+  
+setup( 
+    name='math_quiz', 
+    version='0.1.0', 
+    description='A python package which contains a fun math quiz game', 
+    author='Samarth Kishor Yelvande', 
+    author_email='samarth.yelvande@fau.de', 
+    packages=['math_quiz'], 
+    license='Apache License 2.0'
+)
